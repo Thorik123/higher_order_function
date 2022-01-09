76f98859ec758dbeff41d682c454aad2b9529181
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 19:17:51 +0700

    Merubah

--- test/higher_order_function_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:higher_order_function/higher_order_function.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
